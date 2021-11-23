cf3893c8038893511ccf0737941f1e215fd7331f
Kseniya_ryahova <ryahova09@mail.ru>
2021-11-23 13:38:14 +0300

    Initial Commit

---
+++ Shared/project1App.swift
@@ -0,0 +1,17 @@
+//
+//  project1App.swift
+//  Shared
+//
+//  Created by Ксения Ряхова on 23.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
